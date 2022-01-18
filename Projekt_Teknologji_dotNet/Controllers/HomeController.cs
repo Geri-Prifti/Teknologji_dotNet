3fb1a5369178c82eb3b240787f933661673de62d
Admirim Korici <82025628+admirimkorici@users.noreply.github.com>
2022-01-18 17:15:45 +0100

    Init

--- Projekt_Teknologji_dotNet/Controllers/HomeController.cs
+++ Projekt_Teknologji_dotNet/Controllers/HomeController.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Helpers;
 using System.Web.Mvc;
+using Projekt_Teknologji.NET.Models;
 
-namespace Projekt_Teknologji_dotNet.Controllers
+namespace Projekt_Teknologji.NET.Controllers
 {
     public class HomeController : Controller
     {
@@ -19,10 +21,25 @@ namespace Projekt_Teknologji_dotNet.Controllers
 
             return View();
         }
-
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Contact(Contact c)
+        {
+            if (ModelState.IsValid)
+            {
+                string subject = c.Subjekti;
+                string body = "Derguesi: " + c.Emri + ", Email: " + c.Email + ", Mesazhi: " + c.Mesazhi;
+                string email_rec = "admirimkorici05@gmail.com";
+
+                WebMail.Send(email_rec, subject, body, null, null, null, true, null, null, null, null, null, null);
+                ViewBag.Msg = "Mesazhi u dergua!";
+            }
+            ModelState.Clear();
 
             return View();
         }
---
+++ Projekt_Teknologji_dotNet/Models/Contact.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Projekt_Teknologji.NET.Models
+{
+    [Table("Contact")]
+    public class Contact
+    {
+        [Required(ErrorMessage = "Ju lutem plotesoni fushen!")]
+        public string Emri { get; set; }
+        [Required(ErrorMessage = "Ju lutem plotesoni fushen!")]
+        public string Email { get; set; }
+        public string Subjekti { get; set; }
+        [Required(ErrorMessage = "Shkruani mesazhin tuaj!")]
+        public string Mesazhi { get; set; }
+    }
+}
