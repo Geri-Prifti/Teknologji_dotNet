3d4cdb017638c984352249299db7e7c0ab5db852
Admirim Korici <82025628+admirimkorici@users.noreply.github.com>
2022-01-18 16:56:05 +0100

    Add project files.

---
+++ Projekt_Teknologji_dotNet/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Projekt_Teknologji_dotNet.MvcApplication" Language="C#" %>
---
+++ Projekt_Teknologji_dotNet/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Projekt_Teknologji_dotNet.Startup))]
+namespace Projekt_Teknologji_dotNet
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
